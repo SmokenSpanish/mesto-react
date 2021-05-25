8ae80b0e10c6e24dc70f3395205f0e3b0168ad48
Spanish <maximbasov1992@mail.ru>
2021-05-25 20:04:42 +0300

    портируем App.js в components

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="page">
+      <header className="header">
+            <img src='#' alt="Логотип проекта Mesto" className="header__logo"/>
+        </header>
+
+        <main className="content">
+            <div className="profile">
+                <div className="profile__avatar-wrapper">
+                    <img src="#" alt="аватар" className="profile__avatar"/>
+                    <div className="profile__avatar-overlay"></div>
+                </div>
+                <div className="profile__info">
+                    <h1 className="profile__name">Жак</h1>
+                    <button className="profile__edit-button link" type="button"></button>
+                    <p className="profile__about">Какова твоя профессия</p>
+                </div>
+
+                <button className="profile__add-button link" type="submit">
+                </button>
+            </div>
+
+            <section className="elements">
+            </section>
+        </main>
+
+        <footer className="footer">
+            <p className="footer__logo">&copy; 2020 Mesto Russia</p>
+        </footer>
+    </div>
+  );
+}
+
+export default App;
