3b9aafc497965c48b73eeea1e09c6eeae7bb972a
Spanish <maximbasov1992@mail.ru>
2021-05-26 20:36:15 +0300

    реализация ImagePopup(fin)

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,13 +1,11 @@
 function ImagePopup({ card, onClose }) {
-    const copyCard = Object.assign({}, card);
-
     return(
     <div className={`popup popup-image ${card ? 'popup_opened' : ''}`}>
         <div className="popup__body">
             <div className="popup__image-container popup__overlay">
                 <button onClick={onClose} type="button" className="popup__close-button popup__image-button link" />
-                <img  src={copyCard.link} alt={copyCard.name} className="popup__image"/>
-                <p className="popup__image-title">{copyCard.name}</p>
+                <img  src={card && card.link} alt={card && card.name} className="popup__image"/>
+                <p className="popup__image-title">{card && card.name}</p>
             </div>
         </div>
     </div>
