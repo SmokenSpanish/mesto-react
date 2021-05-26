1559a4966cc846872656e18dcf110793bf9de803
Spanish <maximbasov1992@mail.ru>
2021-05-26 19:10:12 +0300

    реализация запроса данных API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,34 @@
+import React from 'react';
+import api from './utils/Api';
+
 function Main({onEditProfile, onEditAvatar, onAddPlace}) {
+    const [userName, setUserName] = React.useState();
+    const [userDescription, setUserDescription] = React.useState();
+    const [userAvatar, setUserAvatar] = React.useState();
+
+    React.useEffect(() => {
+        Promise.all([api.getUserInfo(), api.getCards()])
+          .then(([userData]) => {
+            setUserName(userData.name);
+            setUserDescription(userData.about);
+            setUserAvatar(userData.avatar);
+          })
+          .catch((err) => {
+            console.log('Один из промисов отклонен', err);
+          });
+      });
+
     return(
         <main className="content">
             <div className="profile">
                 <div onClick={onEditAvatar} className="profile__avatar-wrapper">
-                    <img src="#" alt="аватар" className="profile__avatar"/>
+                    <div style={{backgroundImage: `url(${userAvatar})`}} className="profile__avatar"/>
                     <div className="profile__avatar-overlay" />
                 </div>
                 <div className="profile__info">
-                    <h1 className="profile__name">Жак</h1>
+                    <h1 className="profile__name">{userName}</h1>
                     <button onClick={onEditProfile} className="profile__edit-button link" type="button" />
-                    <p className="profile__about">Исследователь океана</p>
+                    <p className="profile__about">{userDescription}</p>
                 </div>
 
                 <button onClick={onAddPlace} className="profile__add-button link" type="submit" />
